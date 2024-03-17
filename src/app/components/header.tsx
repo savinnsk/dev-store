1e7fc25f1b53c2ee6e2bb46c793617b39ec2205b
54vio <savio.document@gmail.com>
2024-03-17 17:47:30 -0300

    styles: add header styles | style home page

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,44 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Search, ShoppingBag } from 'lucide-react';
+
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <div className='flex items-center justify-between'>
+      <div className='flex items-center gap-5'>
+        <Link className='text-2xl font-extrabold text-white' href='/'>
+          devstore
+        </Link>
+      </div>
+
+      <form className='flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700'>
+        <Search className='h-5 w-5 text-zinc-500' />
+        <input
+          className='outiline-none flex-1 bg-transparent text-sm placeholder:text-zinc-500 '
+          placeholder='Buscar Pordutos...'
+        />
+      </form>
+
+      <div className='flex items-center gap-4'>
+        <div className='flex items-center'>
+          <ShoppingBag className='h-4 w-4' />
+          <span>Carrinho (0)</span>
+        </div>
+        
+          
+        <div className='w-px h-4 bg-zinc-700'/>
+
+        <Link href='/' className='flex items-center gap-2 hover:underline'>
+          <span>Conta</span> 
+          <Image
+            src='https://github.com/savinnsk.png'
+            className='h-6 w-6 rounded-full'
+            width={24}
+            height={24}
+            alt=''
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
